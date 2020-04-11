10a9e2c1ec92f08347f0cdef08be3566163243eb
Aleksey Nikonov <nikonowalex@gmail.com>
2020-04-11 17:58:18 +0300

    Add initial structure.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='radiation_calculator',
+      version='0.1',
+      description='A little library for modelling radiation inside buildings',
+      url='https://github.com/whalecore/radiation_model',
+      author='PMS team',
+      author_email='nikonowalex@gmail.com',
+      license='MIT',
+      packages=['radiation_calculator'],
+      zip_safe=False)
