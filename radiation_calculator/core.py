dc5b4202b1dab36eb9f1e4777d31b380dad27f10
Danil Khayrislamov <danilkhayrislamov@gmail.com>
2020-04-14 14:33:43 +0300

    fixed some bugs in core methods. Added Visualization

--- radiation_calculator/core.py
+++ radiation_calculator/core.py
@@ -9,8 +9,8 @@ def __coordinate_xyz(x, y, z, x0, y0, z0):
 
 def __radiation_at_point(x0, y0, z0, m, l, k, n, P, R, d):
     L = 0
-    for y in range(l, m):
-        for z in range(l, m):
+    for y in range(1, m):
+        for z in range(1, m):
             for x in range(k, k + n):
                 xyz = __coordinate_xyz(x, y, z, x0, y0, z0)
                 L += __radiation_power(P, R) / (xyz * 2 ** (math.sqrt(xyz) * (x - k) / ((x - x0) * d)))
@@ -23,8 +23,8 @@ def __radiation_power(P, R):
 
 def wall_radiation(k, l, m, n, y0, d, P, R):
     radiation_list = []
-    for x0 in range(1, k):
-        for z0 in range(1, l):
+    for x0 in range(1, k - 1):
+        for z0 in range(1, l - 1):
             radiation_list.append(Radiation(x0, z0, __radiation_at_point(x0, y0, z0, m, l, k, n, P, R, d)))
     return radiation_list
 
